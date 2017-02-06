82234b6c8ccf45b81b53ca8e024123f9efe2812f
Ciubotaru-Bordeianu_Andrei <canapoca@yahoo.com>
2017-02-06 12:04:42 +0100

    no message

--- sourcefiles/GXPEngine/MyGame.cs
+++ sourcefiles/GXPEngine/MyGame.cs
@@ -7,11 +7,23 @@ public class MyGame : Game //MyGame is a Game
 	//initialize game here
 	public MyGame () : base(800, 600, false)
 	{
-		
-	}
-	
-	//update game here
-	void Update ()
+        //----------------------------------------------------example-code----------------------------
+        //create a canvas
+        Canvas canvas = new Canvas(800, 600);
+
+        //add some content
+        canvas.graphics.FillRectangle(new SolidBrush(Color.Red), new Rectangle(0, 0, 400, 300));
+        canvas.graphics.FillRectangle(new SolidBrush(Color.Blue), new Rectangle(400, 0, 400, 300));
+        canvas.graphics.FillRectangle(new SolidBrush(Color.Yellow), new Rectangle(0, 300, 400, 300));
+        canvas.graphics.FillRectangle(new SolidBrush(Color.Gray), new Rectangle(400, 300, 400, 300));
+
+        //add canvas to display list
+        AddChild(canvas);
+        //------------------------------------------------end-of-example-code-------------------------
+    }
+
+    //update game here
+    void Update ()
 	{
 		//empty
 	}
